5ebcc109956cf35e14d971d9c9d0297cd62acfee
Ludwig <Ludwig.aab@gmail.com>
2026-03-07 12:13:32 +0100

    Fixed error in example code

--- src/python/pyroquad/examples/LoadingScreen.py
+++ src/python/pyroquad/examples/LoadingScreen.py
@@ -23,8 +23,8 @@ def loading_screen(
     
     ds = screen_width() / 2200
 
-    draw_text(message, 902*ds, 502*ds, 70*ds, Color.ORANGE)
-    draw_text(message, 900*ds, 500*ds, 70*ds, Color.WHITE)
+    draw_text(message,902*ds, 502*ds, Color.ORANGE, font_size= int(70*ds))
+    draw_text(message,900*ds, 500*ds, Color.WHITE, font_size= int(70*ds))
     next_frame(None)
 
     if isinstance(args_list, Sized):
@@ -32,16 +32,16 @@ def loading_screen(
         for i, arg in enumerate(args_list, 1):
             percent = int((i / total) * 100)
 
-            draw_text(message, 902*ds, 502*ds, 70*ds, Color.ORANGE)
-            draw_text(message, 900*ds, 500*ds, 70*ds, Color.WHITE)
-            draw_text(f"{percent}%", 900*ds, 600*ds, 70*ds, Color.WHITE)
+            draw_text(message,902*ds, 502*ds, Color.ORANGE, font_size= int(70*ds))
+            draw_text(message,900*ds, 500*ds, Color.WHITE, font_size= int(70*ds))
+            draw_text(f"{percent}%",900*ds, 600*ds, Color.WHITE, font_size= int(70*ds))
             next_frame(None)
             results.append(func(arg))
     else:
         for i, arg in enumerate(args_list, 1):
-            draw_text(message, 902*ds, 502*ds, 70*ds, Color.ORANGE)
-            draw_text(message, 900*ds, 500*ds, 70*ds, Color.WHITE)
-            draw_text(f"{i}", 900*ds, 600*ds, 70*ds, Color.WHITE)
+            draw_text(message,902*ds, 502*ds, Color.ORANGE, font_size= int(70*ds))
+            draw_text(message,900*ds, 500*ds, Color.WHITE, font_size= int(70*ds))
+            draw_text(f"{i}",900*ds, 600*ds, Color.WHITE, font_size= int(70*ds))
             next_frame(None)
             results.append(func(arg))
 
